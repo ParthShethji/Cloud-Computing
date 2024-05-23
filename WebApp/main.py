15b297028eb824a19436fa69ea08483ec9f4be3c
Parth <shethjiparth1@gmail.com>
2024-05-23 16:03:27 +0530

    Lab-2 Post error

---
+++ WebApp/main.py
@@ -0,0 +1,29 @@
+import webapp2
+from google.appengine.ext.webapp import template
+import os
+import urllib
+
+class MainPage(webapp2.RedirectHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), "index.html")
+        context = {}
+        self.response.out.write(template.render(path, context))
+
+    def post(self):
+        zipcode = self.request.get("zipCode")
+        url = "https://api.postalpincode.in/pincode/" + zipcode
+        data = urllib.urlopen(url).read()
+        post_office = data[0]['PostOffice'][0]['State']
+        name = data[0]['PostOffice'][0]['Name']
+        block = data[0]['PostOffice'][0]['Block']
+        district = data[0]['PostOffice'][0]['District']
+        template_values = {
+            "post_office": post_office,
+            "name": name,
+            "block": block,
+            "district": district
+        }
+        path = os.path.join(os.path.dirname(__file__), "results.html")
+        self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
